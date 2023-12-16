906d737c80c429ef4d104bcee2d69c8549afaba9
Malindu Pabasara <info.malindu4@gmail.com>
2023-12-16 18:01:32 +0530

    added some methods

--- app/Services/BlogService.php
+++ app/Services/BlogService.php
@@ -12,9 +12,9 @@ class BlogService
      * @param Request $request
      * @return Result
      */
-    public function addBlog(Request $request)
+    private function saveBlog(Request $request, $id = null)
     {
-        $blog = new Blog();
+        $blog = $id ? Blog::find($id) : new Blog();
 
         $blog->title = $request->title;
         $blog->blog_content = $request->blog_content;
@@ -26,18 +26,57 @@ class BlogService
         return $blog;
     }
 
+    public function addBlog(Request $request)
+    {
+        return $this->saveBlog($request);
+    }
+
     public function updateBlog(Request $request, $id)
+    {
+        return $this->saveBlog($request, $id);
+    }
+
+    public function getAllBlogs()
+    {
+        $blogs = Blog::all();
+
+        return $blogs;
+    }
+
+    public function getBlog($id)
     {
         $blog = Blog::find($id);
 
-        $blog->title = $request->title;
-        $blog->blog_content = $request->blog_content;
-        $blog->tag = $request->tag;
-        $blog->date = $request->date;
-        $blog->status = $request->status;
-
-        $blog->save();
-
         return $blog;
+    }
+
+    public function disableBlog($id, $status)
+    {
+        $blog = Blog::find($id);
+
+        if ($blog) {
+
+            $blog->status = $status;
+
+            $blog->save();
+
+            return ['message' => 'Status Updated'];
+        }
+
+        return ['error' => 'Blog not found'];
+    }
+
+    public function deleteBlog($id)
+    {
+        $blog = Blog::find($id);
+
+        if ($blog) {
+
+            $blog->delete();
+
+            return ['message' => 'Blog Deleted Id->' . $blog->id];
+        }
+
+        return ['error' => 'Blog not found'];
     }
 }
